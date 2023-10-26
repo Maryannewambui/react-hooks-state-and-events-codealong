da08ac283992e8672166d69f1be1c729da041787
Maryannewambui <trimbleleewambui@gmail.com>
2023-10-26 20:01:41 +0300

    assignment complete

--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -1,13 +1,17 @@
 import React, { useState } from "react";
 
 function Toggle() {
+  //setup initial state
   const [isOn, setIsOn] = useState (false)
+
+  //trigger an update by adding a callback function
   function handleClick () {
     setIsOn((isOn) => !isOn);
   }
   const color = isOn ? "red" : "white";
 
   return (
+    //styling the button
   <button style={{ background: color }} onClick={handleClick}>
       {isOn ? "ON" : "OFF"}
     </button>
